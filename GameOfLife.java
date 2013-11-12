8e58271b1fc96fc2a3042365d77413b93e294a2f
XioSky <adhemar.wissocq@hotmail.fr>
2013-11-12 09:34:26 +0100

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,84 +1,108 @@
+import extensions.Image;
+import extensions.RGBColor;
+
 class GameOfLife extends Program {
-	boolean monde[][] = new boolean[8][8];
+    boolean monde[][] = new boolean[40][40];
+    
 
-	void algorithm() {
-		monde[1][1] = true;
-		monde[2][1] = true;
-		monde[3][1] = true;
-		monde = nextGen(monde);
-		print(monde);
+    void algorithm() {
+	Image img = newImage(600,600);
+	setGrid(img, 40, 40);
+	show(img);
+	monde[4][1] = true;
+	monde[4][2] = true;
+	monde[4][3] = true;
+	monde[3][3] = true;
+	monde[2][2] = true;
+	while(true){
+	    monde = nextGen(monde);
+	    delay(100);
+	    setImage(img, monde);
 	}
+    }
 
-	boolean[][] nextGen(boolean[][] m) {
-		boolean[][] next = monde;
-		// print("next  ");
-		// print(next);
-
-		for (int i = 1; i < length(m, 1) - 1; i++) {
-			for (int j = 1; j < length(m, 2) - 1; j++) {
-				if (m[i][j]) {
-					if (voisins(monde, i, j) < 2) {
-						// print("** : " + voisins(monde, i, j));
-						next[i][j] = false;
-					}
-
-				}
-			}
+    void setImage(Image img, boolean[][] tab){
+	for(int i = 1 ; i < length(tab, 1) -1; i++){
+	    for(int j = 1 ; j < length(tab, 2) -1; j++){
+		if(tab[i][j]){
+		    set(img, i, j, RGBColor.BLACK);
 		}
-		return next;
+		else{
+		    set(img, i, j, RGBColor.WHITE);
+		}
+	    }
 	}
-
-	int voisins(boolean[][] m, int i, int j) {
-		int tmp = 0;
-
-		if (m[i][j - 1]) {
-			tmp++;
+    }
+    
+    boolean[][] nextGen(boolean[][] m) {
+	boolean[][] next = new boolean[length(monde)][length(monde)];
+	
+	
+	for (int i = 1; i < length(m, 1) - 1; i++) {
+	    for (int j = 1; j < length(m, 2) - 1; j++) {
+		if(voisins(monde, i, j) == 3 ){
+		    next[i][j] = true;
 		}
-		if (m[i][j + 1]) {
-			tmp++;
+		if(voisins(monde, i, j) == 2 && monde[i][j]){
+		    next[i][j] = true;
 		}
-		if (m[i - 1][j]) {
-			tmp++;
-		}
-		if (m[i + 1][j]) {
-			tmp++;
-		}
-		if (m[i - 1][j + 1]) {
-			tmp++;
-		}
-		if (m[i + 1][j - 1]) {
-			tmp++;
-		}
-		if (m[i - 1][j - 1]) {
-			tmp++;
-		}
-		if (m[i + 1][j + 1]) {
-			tmp++;
-		}
-
-		return tmp;
+	    }
+	    
 	}
-
-	void print(boolean[][] m) {
-		for (int i = 0; i < length(m, 1); i++) {
-			for (int j = 0; j < length(m, 2); j++) {
-				if (!m[i][j]) {
-					print(".");
-				} else {
-					print("o");
-				}
-			}
-			println();
+	return next;
+    }
+    
+    int voisins(boolean[][] m, int i, int j) {
+	int tmp = 0;
+	
+	if (m[i][j - 1]) {
+	    tmp++;
+	}
+	if (m[i][j + 1]) {
+	    tmp++;
+	}
+	if (m[i - 1][j]) {
+	    tmp++;
+	}
+	if (m[i + 1][j]) {
+	    tmp++;
+	}
+	if (m[i - 1][j + 1]) {
+	    tmp++;
+	}
+	if (m[i + 1][j - 1]) {
+	    tmp++;
+	}
+	if (m[i - 1][j - 1]) {
+	    tmp++;
+	}
+	if (m[i + 1][j + 1]) {
+	    tmp++;
+	}
+	
+	return tmp;
+    }
+    
+    void print(boolean[][] m) {
+	for (int i = 0; i < length(m, 1); i++) {
+	    for (int j = 0; j < length(m, 2); j++) {
+		if (!m[i][j]) {
+		    print(".");
+		} else {
+		    print("o");
 		}
+	    }
+	    println();
 	}
-
-	//
-
-	//
-
-	//
-
-	public static void main(String[] args) {
-		Program.main(args);
+    }
+    
+    //
+    
+    //
+    
+    //
+	public static void main(String[] args){
+		Program.main(args);	
 	}
+	}  
 }
